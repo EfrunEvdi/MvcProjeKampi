620ec03af9990e62f0a4e1b8a333d4d2f89452c3
efrunevdi <efruncetkin@gmail.com>
2023-05-03 16:06:57 +0300

    6. Ders Sonu

--- EntityLayer/Concrete/About.cs
+++ EntityLayer/Concrete/About.cs
@@ -8,5 +8,10 @@ namespace EntityLayer.Concrete
 {
     public class About
     {
+        public int AboutID { get; set; }
+        public string AboutDetails1 { get; set; }
+        public string AboutDetails2 { get; set; }
+        public string AboutImage1 { get; set; }
+        public string AboutImage2 { get; set; }
     }
 }
