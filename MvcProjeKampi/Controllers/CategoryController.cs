e3a51901cee57dea38ca89d61d0b29c254bd35ef
efrunevdi <efruncetkin@gmail.com>
2023-05-15 17:25:18 +0300

    22. Ders Sonu

--- MvcProjeKampi/Controllers/CategoryController.cs
+++ MvcProjeKampi/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,18 @@ namespace MvcProjeKampi.Controllers
 {
     public class CategoryController : Controller
     {
+        CategoryManager cm = new CategoryManager();
+
         // GET: Category
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult GetCategoryList()
+        {
+            var categoryvalues = cm.GelAllBL();
+            return View(categoryvalues);
         }
     }
 }
