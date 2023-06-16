e2ef539852dd9875c2d6a938193c6f709669cc95
efrunevdi <efruncetkin@gmail.com>
2023-06-16 16:34:37 +0300

    70. Ders Sonu

---
+++ MvcProjeKampi/Controllers/LoginController.cs
@@ -0,0 +1,36 @@
+﻿using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Admin admin)
+        {
+            Context context = new Context();
+            var adminuserinfo = context.Admins.FirstOrDefault(x => x.UserName == admin.UserName && x.Password == admin.Password);
+
+            if (adminuserinfo != null)
+            {
+                return RedirectToAction("Index", "AdminCategory");
+            }
+
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
