3bc70852921b63e0454f681fb8bf1e25f155b2f9
efrunevdi <efruncetkin@gmail.com>
2023-06-20 20:37:40 +0300

    74. Ders Sonu

---
+++ MvcProjeKampi/Controllers/WriterPanelController.cs
@@ -0,0 +1,75 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class WriterPanelController : Controller
+    {
+        HeadingManager hm = new HeadingManager(new EfHeadingDal());
+        CategoryManager cm = new CategoryManager(new EfCategoryDal());
+        public ActionResult WriterProfile()
+        {
+            return View();
+        }
+
+        public ActionResult MyHeading()
+        {
+            var values = hm.GetListByWriter();
+            return View(values);
+        }
+
+        [HttpGet]
+        public ActionResult NewHeading()
+        {
+            List<SelectListItem> ValueCategory = (from x in cm.GetList()
+                                                  select new SelectListItem
+                                                  {
+                                                      Text = x.CategoryName,
+                                                      Value = x.CategoryID.ToString()
+                                                  }).ToList();
+
+
+            ViewBag.VLC = ValueCategory;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult NewHeading(Heading heading)
+        {
+            heading.HeadingDate = DateTime.Now;
+            heading.WriterID = 4;
+            heading.HeadingStatus = true;
+            hm.HeadingAdd(heading);
+            return RedirectToAction("MyHeading", "WriterPanel");
+        }
+
+        [HttpGet]
+        public ActionResult EditHeading(int id)
+        {
+            List<SelectListItem> ValueCategory = (from x in cm.GetList()
+                                                  select new SelectListItem
+                                                  {
+                                                      Text = x.CategoryName,
+                                                      Value = x.CategoryID.ToString()
+                                                  }).ToList();
+            ViewBag.VLC = ValueCategory;
+
+            var HeadingValue = hm.GetByID(id);
+            return View(HeadingValue);
+        }
+
+        [HttpPost]
+        public ActionResult EditHeading(Heading heading)
+        {
+            heading.HeadingDate = DateTime.Now;
+            hm.HeadingUpdate(heading);
+            return RedirectToAction("MyHeading", "WriterPanel");
+        }
+    }
+}
